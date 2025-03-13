f1eb8546ee17f738902625539c1dfc587baaa15c
Subhendu <dassubhendu40158@gmail.com>
2025-03-13 10:48:18 +0530

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+
+import path from "path";
+import authRoutes from "./routes/auth.route.js";
+import cors from "cors"
+import { connectDB } from "./lib/db.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+
+dotenv.config()
+
+
+
+
+
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+)
+
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+
+app.use(cookieParser())
+// Increase the payload size limit to 10MB
+
+
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
+
+
+server.listen(PORT, ()=>{
+    console.log("server is running on port:" + PORT);
+    connectDB()
+});
